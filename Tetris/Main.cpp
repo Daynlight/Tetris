540fbf4b9590d0a4d18a1c018d10aac31cd76920
🐢D A Y N L I G H T🐢 <danielstodulski000@gmail.com>
2024-11-16 02:12:27 +0100

    add SDL2 Package

--- Tetris/Main.cpp
+++ Tetris/Main.cpp
@@ -1,10 +1,16 @@
 #include "Game/Game.h"
 
-static const char TITLE[] = "Tetris";
+#include "SDL2/SDL.h"
+
+static const char *TITLE = "Tetris";
 static const char SETTINGSPATH[] = "settings";
 
-int main(){
+int WinMain(){
+  SDL_Window * window = SDL_CreateWindow(TITLE, 0, 0, 500, 500, SDL_WINDOW_SHOWN);
 
+  while(true){
+    
+  }
 
   return 0;
 }
