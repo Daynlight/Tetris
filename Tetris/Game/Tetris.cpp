d93b6bcfa3c60eb4968b5b3b853388f6d3ede9f3
🐢D A Y N L I G H T🐢 <danielstodulski000@gmail.com>
2024-11-19 23:16:39 +0100

    Move Blocks and colorst to assets.cpp

--- Tetris/Game/Tetris.cpp
+++ Tetris/Game/Tetris.cpp
@@ -6,23 +6,6 @@ Tetris::Tetris(Window* window)
   start();
 }
 
-void Tetris::initBlockVariants() {
-  blocks_variants.reserve(7);
-  colour_variants.reserve(5);
-  createBlockVariant({{0,0}, {0,1}, {1,0}, {1,1}});
-  createBlockVariant({{0,0}, {0,1}, {0,2}, {0,3}});
-  createBlockVariant({{0,0}, {1,0}, {0,1}, {-1,1}});
-  createBlockVariant({{0,0}, {-1,0}, {0,1}, {1,1}});
-  createBlockVariant({{0,0}, {0,1}, {0,2}, {1,2}});
-  createBlockVariant({{0,0}, {0,1}, {0,2}, {-1,2}});
-  createBlockVariant({{0,0}, {1,0}, {-1,0}, {0,1}});
-  colour_variants.emplace_back(3, 65, 174);
-  colour_variants.emplace_back(114, 203, 59);
-  colour_variants.emplace_back(255, 213, 0);
-  colour_variants.emplace_back(255, 151, 28);
-  colour_variants.emplace_back(255, 50, 19);
-}
-
 void Tetris::createBlockVariant(std::initializer_list<std::pair<int, int>> list) {
   blocks_variants.emplace_back(window, ROWS/2, 0, list);
 }
